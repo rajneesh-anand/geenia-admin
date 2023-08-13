0d62c7373a3045d01122b89d04b554f0f485ab45
anand.k.rajneesh@gmail.com <anand.k.rajneesh@gmail.com>
2023-08-13 17:19:27 +0530

    form design change

--- src/pages/auth/signin-verify.jsx
+++ src/pages/auth/signin-verify.jsx
@@ -1,5 +1,6 @@
 import Logo from "@components/ui/logo";
 import { getSession } from "next-auth/react";
+import Seo from "@components/common/seo";
 
 export const getServerSideProps = async (context) => {
   const session = await getSession(context);
@@ -19,15 +20,22 @@ export const getServerSideProps = async (context) => {
 
 export default function LoginVerify() {
   return (
-    <div className="flex items-center justify-center h-screen bg-light sm:bg-gray-100">
-      <div className="m-auto max-w-md w-full bg-light sm:shadow p-5 sm:p-8 rounded">
-        <div className="flex justify-center mb-2">
-          <Logo />
+    <>
+      <Seo
+        title="Sign In Verify"
+        description="Admin Dashboard"
+        canonical="/auth/signin-verify"
+      />
+      <div className="flex items-center justify-center h-screen bg-light ">
+        <div className="m-auto max-w-md w-full bg-yellow/25 sm:shadow p-5 sm:p-8 rounded">
+          <div className="flex justify-center mb-2">
+            <Logo />
+          </div>
+          <h3 className="text-center text-green-700  mb-4 mt-4">
+            Login link has been sent to your email address !
+          </h3>
         </div>
-        <h3 className="text-center text-green-900  mb-4 mt-4">
-          Login link has been sent to your email address !
-        </h3>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -4,6 +4,7 @@ import Input from "@components/ui/form/input";
 import { useForm } from "react-hook-form";
 import { signIn, getCsrfToken, getSession } from "next-auth/react";
 import Logo from "@components/ui/logo";
+import Seo from "@components/common/seo";
 
 export default function LoginPage({ csrfToken }) {
   const [error, setError] = useState(null);
@@ -34,57 +35,60 @@ export default function LoginPage({ csrfToken }) {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen bg-light sm:bg-gray-100">
-      <div className="m-auto max-w-md w-full bg-light sm:shadow p-5 sm:p-8 rounded">
-        <div className="flex justify-center mb-2">
-          <Logo />
-        </div>
-        <h3 className="text-center text-cyan-700 text-lg font-semibold mb-4 mt-4">
-          Sign In to Geenia Admin
-        </h3>
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <Input
-            label="Email"
-            type="email"
-            variant="outline"
-            className="mb-4"
-            placeholder="Enter your email address"
-            {...register("email", {
-              required: "You must provide your email address !",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "Invalid email address !",
-              },
-            })}
-            error={errors.email?.message}
-          />
-
-          <div className="relative">
-            <button
-              type="submit"
-              className="inline-flex items-center justify-center w-full  px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-orange/90 rounded-sm shadow-sm focus:outline-none hover:bg-opacity-90"
-            >
-              Sign In
-            </button>
+    <>
+      <Seo
+        title="Admin Sign In"
+        description="Admin Dashboard"
+        canonical="/auth/signin"
+      />
+      <div className="flex items-center justify-center h-screen bg-light ">
+        <div className="m-auto max-w-md w-full bg-yellow/25 sm:shadow p-5 sm:p-8 rounded ">
+          <div className="flex justify-center mb-2">
+            <Logo />
           </div>
-
-          <div className="flex flex-col items-center justify-center relative text-sm text-heading mt-3 sm:mt-4 mb-3 sm:mb-4">
-            <hr className="w-full" />
-          </div>
-
-          {error && (
-            <Alert
-              message={error}
-              variant="error"
-              closeable={true}
-              className="mt-5"
-              onClose={() => setError(null)}
+          <h3 className="text-center text-cyan-700 text-lg font-semibold mb-4 mt-4">
+            Sign In to Geenia Admin
+          </h3>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            <Input
+              label="Email"
+              type="email"
+              variant="outline"
+              className="mb-4"
+              placeholder="Enter your email address"
+              {...register("email", {
+                required: "You must provide your email address !",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Invalid email address !",
+                },
+              })}
+              error={errors.email?.message}
             />
-          )}
-        </form>
+
+            <div className="relative">
+              <button
+                type="submit"
+                className="inline-flex items-center justify-center w-full  px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-orange/90 rounded-sm shadow-sm focus:outline-none hover:bg-opacity-90"
+              >
+                Sign In
+              </button>
+            </div>
+
+            {error && (
+              <Alert
+                message={error}
+                variant="error"
+                closeable={true}
+                className="mt-5"
+                onClose={() => setError(null)}
+              />
+            )}
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
